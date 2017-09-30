bbd7cffb574359cbe109457e2ee80c5046a5880e
robinsonvs <robinsonvs@gmail.com>
2017-09-30 20:50:09 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DataTableModule, ButtonModule, InputTextModule, InputMaskModule, CalendarModule } from 'primeng/primeng';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ItemService } from './item/item.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ItemCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTableModule,
+    HttpClientModule,
+    ButtonModule,
+    InputMaskModule,
+    CalendarModule,
+    InputTextModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ItemService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
